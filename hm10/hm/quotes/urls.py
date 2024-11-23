64f42f3330bec85f75803caaf8d2531f23932253
dima cheban <dimacheban23@gmail.com>
2024-11-23 10:47:51 +0200

    создал новый проект command: django-admin startproject mysyte . startproject - создать проект mysyte - название папки, точки входа проета . - создать в текущей директории

--- hm10/hm/quotes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='quotes'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('<int:page>', views.home, name='root_paginate'),
-]
