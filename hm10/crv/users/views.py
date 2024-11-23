d8ef579026ca142dd54d861fa6be244615990e85
dima cheban <dimacheban23@gmail.com>
2024-11-23 17:33:07 +0200

    добавил возможность регистрации и логинизации пользователя
    после успешного входа в систему перебрасывает на домашнюю странцу

--- hm10/crv/users/views.py
+++ hm10/crv/users/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm, LoginForm
 
-# Create your views here.
+
 
 def register_and_login(request):
-    return render(request,'users/register_login.html', context={'text':'Register and loging page'})
+
+    registration_form = RegisterForm()
+    login_form = LoginForm()
+
+    if request.method == 'POST':
+        if 'register' in request.POST:
+            registration_form  =  RegisterForm(request.POST)
+            if registration_form.is_valid():
+                user = registration_form.save()
+                login(request, user)
+                return redirect('quotes:home') # перенаправление на домашнюю страницу
+
+        elif 'login' in request.POST:
+            login_form = LoginForm(request, request.POST)
+            if login_form.is_valid():
+                username = login_form.cleaned_data.get('username')
+                password = login_form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('quotes:home')
+
+    context = {
+        'registration_form': registration_form,
+        'login_form': login_form,
+    }
+    return render(request, 'users/register_login.html', context)
