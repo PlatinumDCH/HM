b125562d8a8fbe64f64d3d7169f496e7d4dbb75f
dima cheban <dimacheban23@gmail.com>
2024-11-25 11:14:51 +0200

    создал пагинатор

--- hm10/crv/quotes/views.py
+++ hm10/crv/quotes/views.py
@@ -3,9 +3,14 @@ from .models import Quotes
 from django.core.paginator import Paginator
 
 
-def home_page(request):
+def home_page(request, page=1):
+
     quotes = Quotes.objects.all()
-    return render(request, 'quotes/home.html', {'quotes':quotes})
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page)
+    quotes_on_page = paginator.page(page)
+
+    return render(request, 'quotes/home.html', {'quotes':quotes_on_page})
 
 def search(request):
     context = {
