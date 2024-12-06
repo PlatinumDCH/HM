c180717e9f513d51a2067ce4038846f7b37e81eb
dima cheban <dimacheban23@gmail.com>
2024-12-06 16:46:12 +0200

    сгенерировать новый секретный ключ - openssl rand -hex 32 закинул его в конф файл доступен из srv/conf/config.py также закин вид алгоритма в конфигурационный файл

--- hm11/srv/conf/config.py
+++ hm11/srv/conf/config.py
@@ -5,7 +5,7 @@ import os
 config_file_path = os.path.join(os.path.dirname(__file__),'config.ini')
 logger = setup_logger(__name__)
 
-def load_db_config(config_file=config_file_path,section='PG'):
+def load_config(config_file=config_file_path,section='PG'):
     parser = ConfigParser()
     parser.read(config_file)
 
@@ -21,15 +21,29 @@ def load_db_config(config_file=config_file_path,section='PG'):
         raise Exception(f'Section {section} not found in {config_file}')
 
 try:
-    db_config = load_db_config()
+    db_config = load_config()
     db_url = f"{db_config['driver']}://{db_config['username']}:{db_config['password']}@" \
              f"{db_config['host']}:{db_config['port']}/{db_config['database']}"
 except Exception as err:
     logger.error(f'Failed to create DB URL: {err}')
     raise
 
+try:
+    key_config = load_config(section='KEY')
+    SECRET_KEY = key_config['secret_key']
+except Exception as err:
+    logger.error(f'Failed to load secret key: {err}')
+    raise
+
+try:
+    algorithm_config = load_config(section='ALGO')
+    ALGORITHM = key_config['algorithm']
+except Exception as err:
+    logger.error(f'Failed to load ALGORITHM: {err}')
+    raise
 
 class Config:
     DB_URL = db_url
 
 config = Config
+
--- hm11/srv/services/auth.py
+++ hm11/srv/services/auth.py
@@ -12,10 +12,13 @@ import pytz
 
 from srv.database.db import get_db
 from srv.repository import users as repository_users
+from srv.conf.config import SECRET_KEY as KEY
+from srv.conf.config import ALGORITHM as ALGO
 
 class Auth:
     pwd_context = CryptContext(schemes = 'bcrypt',deprecated = 'auto',bcrypt__rounds = 6)
-
+    SECRET_KEY = KEY
+    ALGORITHM = ALGO
 
 
 
