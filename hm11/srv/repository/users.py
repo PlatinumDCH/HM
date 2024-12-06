ecbc1551b059c4133374d7e9d6025b5b5d1c56ee
dima cheban <dimacheban23@gmail.com>
2024-12-06 18:51:55 +0200

    функция создает пользователя в бд возвращает объект new_user

--- hm11/srv/repository/users.py
+++ hm11/srv/repository/users.py
@@ -1,13 +1,31 @@
 from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from libgravatar import Gravatar
 
 from srv.database.db import get_db
 from srv.entity.models import User
+from srv.schemas.user import UserSchema
+from srv.conf.loging_conf import setup_logger
 
+logger = setup_logger(__name__)
 
 async def get_user_by_email(email:str, db:AsyncSession=Depends(get_db)):
     user_query = select(User).filter_by(email=email)
     user = await db.execute(user_query)
     user = user.scalar_one_or_none()
     return user
+
+
+async def create_user(body:UserSchema, db:AsyncSession=Depends(get_db)):
+    avatar = None
+    try:
+        g = Gravatar(body.email)
+        avatar = g.get_image()
+    except Exception as err:
+        logger.error(err)
+    new_user = User(**body.model_dump(), avatar=avatar)
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    return new_user
--- hm11/srv/routes/auth.py
+++ hm11/srv/routes/auth.py
@@ -10,10 +10,17 @@ from srv.services.auth import auth_service
 router = APIRouter(prefix='/auth', tags=['auth'])
 get_refresh_token = HTTPBearer()
 
-@router.post('/signup')
+@router.post('/signup', response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def signup(body: UserSchema, db:AsyncSession=Depends(get_db)):
-    pass
-    return  {}
+    #проверка на то что такого пользователя нету в БД
+    exists_user = await repository_users.get_user_by_email(body.email, db)
+    if exists_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail='Account already exists')
+
+    #создаем нового пользователя
+    body.password = auth_service.get_pass_hash(body.password)
+    new_user = await repository_users.create_user(body, db)
+    return new_user
 
 @router.post('/login')
 async def login(body: OAuth2PasswordRequestForm=Depends(), db:AsyncSession=Depends(get_db)):
