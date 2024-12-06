6a0a9e669ff5df87d0aafdc50d10cb9c9d9812e5
dima cheban <dimacheban23@gmail.com>
2024-12-06 18:38:54 +0200

    добавил нужные импорты get_refresh_token - это экземпляр класса HTTPBearer, который использует заголовки HTTP-заголовков авторизации. Предоставляет механизм аутентификации через токены, передаваемые в заголовке Authorization запроса
    он ожидает что заголовок будет содержать строку в формате
    Authorization: Bearer <token>
    Если заголовок отсутствует или токен передан некорректно, FastAPI автоматически вернёт ответ с кодом 401 Unauthorized
    
    тип возвращаемого значения
    Если токен найден и валиден по формату get_refresh_token вернёт объект типа HTTPAuthorizationCredentials, содержащий
    scheme - схему аутентификации
    credentials - сам токен(строка)

--- hm11/srv/routes/auth.py
+++ hm11/srv/routes/auth.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, HTTPException, Depends, status, Path, Query, Security
-from fastapi.security import OAuth2PasswordRequestForm, HTTPAuthorizationCredentials
+from fastapi.security import OAuth2PasswordRequestForm, HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from srv.database.db import get_db
-from srv.schemas.user import UserSchema
+from srv.repository import users as repository_users
+from srv.schemas.user import UserSchema, TokenSchema, UserResponse
+from srv.services.auth import auth_service
 
 router = APIRouter(prefix='/auth', tags=['auth'])
+get_refresh_token = HTTPBearer()
 
 @router.post('/signup')
 async def signup(body: UserSchema, db:AsyncSession=Depends(get_db)):
