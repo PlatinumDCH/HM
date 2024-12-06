2aeb4b14f41c705a1779abc48bd1a11da6a39591
dima cheban <dimacheban23@gmail.com>
2024-12-06 20:02:53 +0200

    добавил обновление refresh token

--- hm11/srv/routes/auth.py
+++ hm11/srv/routes/auth.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, HTTPException, Depends, status, Path, Query, Security
 from fastapi.security import OAuth2PasswordRequestForm, HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from srv.database.db import get_db
+from srv.entity.models import UserToken
 from srv.repository import users as repository_users
 from srv.schemas.user import UserSchema, TokenSchema, UserResponse
 from srv.services.auth import auth_service
@@ -30,12 +32,27 @@ async def login(body: OAuth2PasswordRequestForm=Depends(), db:AsyncSession=Depen
     if not auth_service.verify_pass(body.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid pass')
     #создание токена
-    access_token = await auth_service.create_access_token(data={'sub': user.email})
-    refresh_token = await auth_service.create_refresh_token(data={'sub': user.email})
-    await repository_users.update_token(user, refresh_token, db)
-    return {'access_token': access_token, 'refresh_token':refresh_token, 'token_type':'bearer'}
+    new_access_token = await auth_service.create_access_token(data={'sub': user.email})
+    new_refresh_token = await auth_service.create_refresh_token(data={'sub': user.email})
+    await repository_users.update_token(user, new_refresh_token, db)
+    return {'access_token': new_access_token, 'refresh_token':new_refresh_token, 'token_type':'bearer'}
 
-@router.get('/refresh_token')
-async def refresh_token(credentials:HTTPAuthorizationCredentials=Security(),db:AsyncSession=Depends(get_db)):
-    pass
-    return {}
+@router.get('/refresh_token',response_model=TokenSchema)
+async def refresh_token(credentials:HTTPAuthorizationCredentials=Security(get_refresh_token),
+                        db:AsyncSession=Depends(get_db)):
+    token = credentials.credentials #получаем refresh token из БД
+    email = await auth_service.decode_refresh_token(token) #декодируем refresh token, получаем email
+    user = await repository_users.get_user_by_email(email, db)
+    user_token_query = await db.execute(
+        select(UserToken).filter_by(user_id=user.id)
+    )
+    user_refresh_token = user_token_query.scalar_one_or_none()
+    if not user_refresh_token or user_refresh_token != token:
+        if user_refresh_token:
+            await repository_users.update_token(user, token, db)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid refresh token')
+    # как новый логин, спрятано от пользователя
+    new_access_token = await auth_service.create_access_token(data={'sub': user.email})
+    new_refresh_token = await auth_service.create_refresh_token(data={'sub':user.email})
+    await repository_users.update_token(user, new_refresh_token,db)
+    return {'access_token': new_access_token,'refresh_token':new_refresh_token,'token_type':'bearer'}
