cd20d6c72019c3e5cbd9a0f8e31277948164425e
dima cheban <dimacheban23@gmail.com>
2024-12-06 19:25:33 +0200

    добавил привязку юзеров до роутов контактов

--- hm11/srv/routes/contacts.py
+++ hm11/srv/routes/contacts.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter, HTTPException, Depends, status, Path, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from srv.database.db import get_db
+from srv.entity.models import User
 from srv.repository import contacts as repositories_contacts
 from srv.schemas.contacts import ContactCreateSchema, ContactResponse
+from srv.services.auth import auth_service
 
 router = APIRouter(prefix='/contacts', tags=['contacts'])
 
@@ -12,7 +14,8 @@ router = APIRouter(prefix='/contacts', tags=['contacts'])
 async def get_contacts(
         limit: int = Query(10, ge=10, le=500),
         offset: int = Query(0, ge=0),
-        db: AsyncSession = Depends(get_db)):
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(auth_service.get_current_user)):
     """ Retrieve a list of contacts with pagination. """
     contacts = await repositories_contacts.get_contacts(limit, offset, db)
     return contacts
@@ -22,7 +25,8 @@ async def search_contacts(
         first_name:str = Query(None, description='Optional first name to search'),
         last_name:str = Query(None, description='Optional last name to search'),
         email:str = Query(None, description='Optional email to search'),
-        db:AsyncSession = Depends(get_db)):
+        db:AsyncSession = Depends(get_db),
+        user: User = Depends(auth_service.get_current_user)):
     """Search for contacts by first name, last name, or email. """
     if not any([first_name, last_name, email]):
         raise HTTPException(
@@ -39,7 +43,8 @@ async def search_contacts(
     return contacts
 
 @router.get('/upcoming_birthdays', response_model=list[ContactResponse])
-async def upcoming_birthdays(db: AsyncSession = Depends(get_db)):
+async def upcoming_birthdays(db: AsyncSession = Depends(get_db),
+                             user: User = Depends(auth_service.get_current_user)):
     """ Retrieve contacts with upcoming birthdays within the next 7 days. """
     upcoming_contacts = await repositories_contacts.get_upcoming_birthdays(db)
     if not upcoming_birthdays:
@@ -51,7 +56,8 @@ async def upcoming_birthdays(db: AsyncSession = Depends(get_db)):
 @router.get("/{contact_id}", response_model=ContactResponse)
 async def get_contact(
         contact_id: int = Path(ge=1),
-        db: AsyncSession = Depends(get_db)):
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(auth_service.get_current_user)):
     """ Get contact object from database """
     contact = await repositories_contacts.get_contact(contact_id, db)
     if contact is None:
@@ -63,7 +69,8 @@ async def get_contact(
 @router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
 async def create_contact(
         body: ContactCreateSchema,
-        db: AsyncSession = Depends(get_db)):
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(auth_service.get_current_user)):
     """ Create contact object """
     contact = await repositories_contacts.create_contact(body, db)
     return contact
@@ -72,7 +79,8 @@ async def create_contact(
 async def update_contact(
         body: ContactCreateSchema,
         contact_id: int = Path(ge=1),
-        db: AsyncSession = Depends(get_db)):
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(auth_service.get_current_user)):
     """ Update contact object"""
     contact = await repositories_contacts.update_contact(contact_id, body, db)
     if contact is None:
@@ -82,7 +90,8 @@ async def update_contact(
 @router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_contact(
         contact_id: int = Path(ge=1),
-        db: AsyncSession = Depends(get_db)):
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(auth_service.get_current_user)):
     contact = await repositories_contacts.delete_contact(contact_id, db)
     return contact
 
