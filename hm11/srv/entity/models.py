ca82ea789577d24c9cced70689bd0375580f681e
dima cheban <dimacheban23@gmail.com>
2024-12-06 15:52:30 +0200

    импортировал необходимые библиотеки обновил модель, контакты.Добавил поля [создание и обновление записи] Связал таблицу контакты и пользователь добавил новую таблицу рефреш токен для юзера

--- hm11/srv/entity/models.py
+++ hm11/srv/entity/models.py
@@ -1,19 +1,42 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Date
+from datetime import date
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Date, func, DateTime, ForeignKey, Integer
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
 
 class Contact(Base):
+    __tablename__               = 'contact'
+    id: Mapped[int]             = mapped_column(primary_key=True,index=True)
+    first_name:Mapped[str]      = mapped_column(String(50))
+    last_name:Mapped[str]       = mapped_column(String(50))
+    email:Mapped[str]           = mapped_column(String(25))
+    phone_number:Mapped[str]    = mapped_column(String)
+    date_birthday:Mapped[Date]  = mapped_column(Date)
+    note:Mapped[str]            = mapped_column(String, default=None)
+    created_at: Mapped[date]    = mapped_column('created_at',DateTime,default=func.now(),
+                                nullable=True)
+    updated_at: Mapped[date]    = mapped_column('updated_at',DateTime,default=func.now(),
+                                onupdate=func.now(),nullable=True)
+    users_id: Mapped[int]       = mapped_column(Integer,ForeignKey('users.id'),nullable=True)
+    user: Mapped['User']        = relationship('User', backref='todos', lazy='joined')
 
-    __tablename__                  = 'contact'
-    id: Mapped[int]                = mapped_column(primary_key=True,index=True)
-    first_name:Mapped[str]         = mapped_column(String(50))
-    last_name:Mapped[str]          = mapped_column(String(50))
-    email:Mapped[str]              = mapped_column(String(25))
-    phone_number:Mapped[str]       = mapped_column(String)
-    date_birthday:Mapped[Date]     = mapped_column(Date)
-    note:Mapped[str]               = mapped_column(String, default=None)
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int]          = mapped_column(primary_key=True)
+    username: Mapped[str]    = mapped_column(String(50))
+    email: Mapped[str]       = mapped_column(String(150), nullable=False, unique=True)
+    password: Mapped[str]    = mapped_column(String(255), nullable=False)
+    avatar: Mapped[str]      = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(),
+                                             onupdate=func.now())
 
-
+class UserToken(Base):
+    __tablename__              = 'user_token'
+    id: Mapped[int]            = mapped_column(primary_key=True,index=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    user_id: Mapped[int]       = mapped_column(Integer, ForeignKey='users_id',nullable=False)
+    user: Mapped['User']       = relationship('User', backref='tokens',lazy='joined')
