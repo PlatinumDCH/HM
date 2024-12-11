9ac7e5308dc9c1b3fde8af59a4c3dd9bf137a2f9
dima cheban <dimacheban23@gmail.com>
2024-12-11 15:09:45 +0200

    rabbitMQ service: -- добавил логику обработки ошибок -- если будут 5 критических ошибок на уровне подключения к сервису.Воркер завершит работу

--- hm11/rabbit_mq_service/rabbitmq_worker.py
+++ hm11/rabbit_mq_service/rabbitmq_worker.py
@@ -15,6 +15,7 @@ from srv.conf.loging_conf import global_logger as logger
 
 BASE_DIR = Path(__file__).resolve().parents[1]
 
+#настройки для FastAPI Mail
 conf = ConnectionConfig(
     MAIL_USERNAME=configuration.MAIL_USERNAME,
     MAIL_PASSWORD=configuration.MAIL_PASSWORD,
@@ -30,6 +31,7 @@ conf = ConnectionConfig(
 )
 
 async def process_message(message:IncomingMessage):
+    """Обработка входящего сообщения из очереди"""
     try:
         async with message.process():
             task_data = json.loads(message.body)
@@ -37,7 +39,10 @@ async def process_message(message:IncomingMessage):
             username = task_data['username']
             host = task_data['host']
 
+            # создание токена для подтверждения email
             token_verification = auth_service.create_email_token({'sub':email})
+
+           # формирование сообщения
             message_schema = MessageSchema(
                 subject='Confirm your email',
                 recipients=[email],
@@ -52,9 +57,14 @@ async def process_message(message:IncomingMessage):
         raise
 
 async def main():
-    while True:
+    """Основной воркер для обработки сообщений из RabbitMQ"""
+    max_retires = 5 # максимальное количество повторных попыток
+    retry_count = 0 # Счетчик попыток
+    while retry_count < max_retires:
         try:
             connection = await get_rabbitmq_connection()
+
+            # Объявление обменника
             async with connection:
                 channel = await connection.channel()
                 exchange = await channel.declare_exchange(
@@ -62,17 +72,24 @@ async def main():
                         type=ExchangeType.DIRECT,
                         durable=True
                     )
+
+                # Объявление очереди
                 queue = await channel.declare_queue('email_queue', durable=True)
                 await queue.bind(exchange, routing_key="email_queue")
                 await queue.consume(process_message)
                 logger.info("Worker is consuming messages...")
+
+                # Ожидание новых сообщений
                 while True:
                     await asyncio.sleep(1)
         except Exception as err:
+            retry_count += 1
             logger.error(f'An error occurred in the main loop: {err}')
-            logger.info('Retrying in 5 seconds...')
-            await asyncio.sleep(5)
-
+            if retry_count < max_retires:
+                logger.info(f'Retrying in 5 seconds... Attempt {retry_count}/{max_retires}')
+                await asyncio.sleep(5)
+            else:
+                logger.critical('Max retry attempts reached.Exiting')
 if __name__ == '__main__':
     asyncio.run(main())
 
